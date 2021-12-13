24df8cf41dee3c4f603d7d31459bbd3412a51656
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-12-13 11:17:54 -0300

    Fluro Transition

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,14 +1,22 @@
-
+import 'package:bases_web/ui/views/counter_provider_view.dart';
 import 'package:bases_web/ui/views/counter_view.dart';
 import 'package:fluro/fluro.dart';
 
 class Flurorouter {
   static final FluroRouter router = FluroRouter();
   static void configureRoute() {
-    router.define('/', handler: _counterHandler);
+    router.define('/',
+        handler: _counterHandler, transitionType: TransitionType.fadeIn);
+    router.define('/stateful',
+        handler: _counterHandler, transitionType: TransitionType.fadeIn);
+    router.define('/provider',
+        handler: _counterProviderHandler,
+        transitionType: TransitionType.fadeIn);
   }
 
-  //Handlers- Manejadores
+  //Handlers - Manejadores
   static final Handler _counterHandler =
       Handler(handlerFunc: (context, params) => const CounterView());
+  static final Handler _counterProviderHandler =
+      Handler(handlerFunc: (context, params) => const CounterProviderView());
 }
